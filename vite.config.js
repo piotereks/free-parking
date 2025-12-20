09dd81e6660c38d61507f345e93f044b2669a15c
Piotr Sakowski <piotereks@gmail.com>
2025-12-20 19:54:05 +0100

    change of deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
 
   build: {
     emptyOutDir: true,
+    outDir: path.resolve(__dirname, 'parking-deploy/docs/html/parking'),
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
