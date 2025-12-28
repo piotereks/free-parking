6b30eeeeb26bf09a950b28dab81185dde42a073e
Piotr Sakowski <piotereks@gmail.com>
2025-12-28 14:25:50 +0100

    android app run attemp1

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,5 +1,11 @@
+import 'react-native-url-polyfill/auto';
 import { registerRootComponent } from 'expo';
 import App from './src/App';
+
+// Hermes on Android doesn't expose SharedArrayBuffer; stub to satisfy libs that probe it
+if (typeof global.SharedArrayBuffer === 'undefined') {
+	global.SharedArrayBuffer = ArrayBuffer;
+}
 
 // Register the root component
 registerRootComponent(App);
