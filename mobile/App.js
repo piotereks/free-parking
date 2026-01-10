d7375e1c1d34b9344a0f1166556822a28fd878e3
Piotr Sakowski <piotereks@gmail.com>
2026-01-10 18:33:58 +0100

    first admob commit (wip_

--- mobile/App.js
+++ mobile/App.js
@@ -1,3 +1,28 @@
+import React from 'react';
+import { SafeAreaView, View, StyleSheet } from 'react-native';
 import App from './src/App';
+import { BannerAd, BannerAdSize } from 'react-native-google-mobile-ads';
 
-export default App;
+export default function Root() {
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.content}>
+				<App />
+			</View>
+			<View style={styles.bannerContainer}>
+				<BannerAd
+					unitId="ca-app-pub-3940256099942544/9214589741"
+					size={BannerAdSize.ADAPTIVE_BANNER}
+				/>
+			</View>
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: { flex: 1 },
+	content: { flex: 1 },
+	bannerContainer: {
+		alignSelf: 'stretch',
+	},
+});
