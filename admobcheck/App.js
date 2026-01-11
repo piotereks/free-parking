5b6be51e9e8acf51968704b0d5edcc0b76b9cabe
Piotr Sakowski <piotereks@gmail.com>
2026-01-11 01:55:22 +0100

    wip

--- admobcheck/App.js
+++ admobcheck/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AdMobManager from './AdMobManager';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <AdMobManager style={{ marginTop: 10 }} />
     </View>
   );
 }
