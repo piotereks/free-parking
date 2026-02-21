9ed07ac76616ab1842cc0a4310c49ede1b85f9c3
Piotr Sakowski <piotereks@gmail.com>
2026-02-22 00:31:01 +0100

    feat: persistence implementation

---
+++ mobile/__mocks__/expo-secure-store.js
@@ -0,0 +1,5 @@
+// manual mock for expo-secure-store
+export const setItemAsync = jest.fn();
+export const getItemAsync = jest.fn();
+export const deleteItemAsync = jest.fn();
+export const isAvailableAsync = jest.fn();
